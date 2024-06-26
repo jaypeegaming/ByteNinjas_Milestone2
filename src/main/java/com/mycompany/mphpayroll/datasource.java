1aee303825f19525c176dc01d71f36dda2b8efbd
jaypeegaming <lr.jpbalmaceda@mmdc.mcl.edu.ph>
2024-06-26 19:47:58 +0800

    Milestone 2 |MotorPH employee application with record delete Functionality

---
+++ src/main/java/com/mycompany/mphpayroll/datasource.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mphpayroll;
+
+/**
+ *
+ * @author AXISMEG
+ */
+public class datasource {
+    
+    public static int logemp;
+    
+    
+}
